4a5c664517fbfb9493c46e66c236b9d2dcc6c47a
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-07-25 21:57:46 -0500

    suma

---
+++ main.js
@@ -0,0 +1,6 @@
+let nombre = prompt(`hola, cual es tu nombre?`);
+let num1 = parseInt(prompt(`${nombre}, ingresa el primer valor `));
+let num2 = parseInt(prompt(`${nombre}, ingresa el segundo  valor `));
+
+let resultado = num1 + num2
+alert(`${nombre}, el resultado de tu suma es ${resultado}`)
